87ef5d14497d411a9c2535dad306f8307163c6d7
Charly <charlheinz@gmx.de>
2018-08-17 21:03:42 +0200

    Update de.js

--- lang/de.js
+++ lang/de.js
@@ -1,13 +1,13 @@
 L10N = {
 	"gallery": "Galerie",
-	"startScreen": "<h1>Photobooth<\/h1><h2>Webinterface<\/h2> by Andr\u00e9 Rinas",
+	"startScreen": "<h1>Hochzeits Fotobox<\/h1><h2>von<\/h2><h2>Verena & Charly",
 	"takePhoto": "Foto erstellen!",
 	"home": "Home",
 	"qr": "QR Code",
 	"newPhoto": "Neues Bild",
 	"busy": "Bild wird verarbeitet",
 	"cheese": "Cheeeeeeeese!",
-	"qrHelp": "Um das Bild auf dein Handy zu laden, verbinde dich mit dem WLAN <strong>photobooth<\/strong>",
+	"qrHelp": "Um das Bild auf dein Handy zu laden, verbinde dich mit dem WLAN <strong>Fotobox<\/strong> Passwort:<strong>justmarried<\/strong>",
 	"error": "Es ist ein Fehler aufgetreten, bitte versuche es erneut",
 	"reload": "Seite neu laden",
 	"print": "Drucken"
